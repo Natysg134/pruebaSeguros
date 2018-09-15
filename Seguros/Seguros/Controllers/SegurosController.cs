f017ed01515e4d1148cde98633c842f3a9bb4390
Natalia <natysg134@gmail.com>
2018-09-14 21:18:15 -0600

    Controller con get de prueba

---
+++ Seguros/Seguros/Controllers/SegurosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Seguros.Controllers
+{
+    public class SegurosController : ApiController
+    {
+        private static List<string> abc = new List<string> { "a", "b", "c" };
+        
+        [HttpGet]
+        public IEnumerable<string> GetLetters()
+        {
+            return abc;
+        }
+    }
+}
---
+++ Seguros/Seguros/Models/Seguro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seguros.Models
+{
+    public class Seguro
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cubrimiento { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public int Meses { get; set; }
+        public decimal Precio { get; set; }
+        public string TipoRiesgo { get; set; }
+    }
+}
